f25da55d1b1d480123ebe69d4a9d97fdadd9bca3
SipS0p <sipsopgamera@gmail.com>
2023-11-16 11:07:45 +0100

    1. and 2. Done
    1. Spawn an object anywhere between the pipes and it moves on the screen together with everything else.
    
    2. Spawn the object in a random X and Y position between the pipes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,10 +6,19 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject PipeToSpawn;
+    [SerializeField] private GameObject coin;
+
     private float TimeUntilSpawn = 3.5f;
-    private float TimeBetweenSpawns = -13.62f;
+    private float TimeBetweenSpawns;
     private float MinSpawnHeight = -13.62f;
     private float MaxSpawnHeight = 6f;
+
+    private float coinMinSpawnHeight = -16f;
+    private float coinMaxSpawnHeight = 16f;
+    private float coinMinSpawnWidth = 36.6f;
+    private float coinMaxSpawnWidth = 46f;
+
+    private bool alreadySpawnedCoin;
 
     private void Start()
     {
@@ -30,14 +39,25 @@ public class Spawner : MonoBehaviour
             // Instantiate a new PipeToSpawn GameObject at the calculated spawn position
             Instantiate(PipeToSpawn, spawnPosition, Quaternion.identity);
 
+            alreadySpawnedCoin = false;
             // Reset the TimeUntilSpawn to its initial value
-            TimeUntilSpawn =TimeBetweenSpawns;
+            TimeUntilSpawn = TimeBetweenSpawns;
 
             // Decrease the TimeBetweenSpawn, gradually making spawning faster
             if (TimeBetweenSpawns > 2.3f)
             {
                 TimeBetweenSpawns -= 0.075f;
             }
+        }
+        else if(TimeUntilSpawn < TimeBetweenSpawns/ 2 && !alreadySpawnedCoin)
+        {
+            //Spawn coin
+            float randomY = Random.Range(coinMinSpawnHeight, coinMaxSpawnHeight);
+            float randomX = Random.Range(coinMinSpawnWidth, coinMaxSpawnWidth);
+
+            Vector2 spawnPosition = new Vector2(randomX, randomY);
+            Instantiate(coin, spawnPosition, Quaternion.identity);
+            alreadySpawnedCoin = true;
         }
         else
         {
